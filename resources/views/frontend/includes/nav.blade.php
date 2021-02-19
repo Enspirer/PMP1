24bf3051de8c65b7c4be64662aaeddd65a364d8a
kavindutheekshana@gmail.com <Kavindu1997>
2021-02-19 18:36:40 +0530

    Logo and Favicon

--- resources/views/frontend/includes/nav.blade.php
+++ resources/views/frontend/includes/nav.blade.php
@@ -2,7 +2,12 @@
 <section class="nav-section">
     <nav class="navbar-main navbar navbar-expand-lg navbar-light">
         <div class="container">
-            <a class="navbar-brand" href="{{url('/')}}">LOGO</a>
+            <a class="navbar-brand" style="position: absolute" href="{{url('/')}}"><img
+                    src="assets/image/logonew.png"
+                    alt=""
+                    width="70px"
+                    style="position: absolute; top: -11px"
+                /></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
                     aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -14,6 +14,7 @@
     <!-- Required meta tags -->
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+    <link rel="icon" type="image/png" href="assets/image/favicon.png" />
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w==" crossorigin="anonymous"/>
