8ee26f299295bb464776954d5eb06e6a8ab062b2
Sanjaya Senevirathne <sanjayaharasana@gmail.com>
2021-02-23 17:00:19 +0530

    Chat Section Script

--- public/theme_light/charts/chart_section.js
+++ public/theme_light/charts/chart_section.js
@@ -2,252 +2,88 @@ var dom = document.getElementById("container");
 var myChart = echarts.init(dom);
 var app = {};
 var option;
-var timeData = [
-    "12.00 AM",
-    "01.00 AM",
-    "02.00 AM",
-    "03.00 AM",
-    "04.00 AM",
-    "05.00 AM",
-    "06.00 AM",
-    "07.00 AM",
-    "08.00 AM",
-    "09.00 AM",
-    "10.00 AM",
-    "11.00 AM",
-    "12.00 AM",
-    "01.00 PM",
-    "02.00 PM",
-    "03.00 PM",
-    "04.00 PM",
-    "05.00 PM",
-    "06.00 PM",
-    "07.00 PM",
-    "08.00 PM",
-    "09.00 PM",
-    "10.00 PM",
-    "11.00 PM",
-    "12.00 PM",
-];
-var bidsData = [
-    10,
-    30,
-    29,
-    49,
-    50,
-    28,
-    20,
-    50,
-    29,
-    40,
-    29,
-    30,
-    10,
-    30,
-    23,
-    49,
-    65,
-    13,
-];
-
-//setInterval(function() {
-//
-//},  60 * 1000); // 60 * 1000 milsec
-
-timeData = timeData.map(function (str) {
-    return str.replace("2009/", "");
-});
-
 option = {
-    title: {
-        text: "Live Bids",
-        left: "center",
-        textStyle: {
-            color: "#FFFF",
-            fontSize: 18,
-        },
-    },
     tooltip: {
-        trigger: "axis",
+        trigger: 'axis',
         axisPointer: {
-            animation: false,
-        },
-    },
-    legend: {
-        data: ["流量", "降雨量"],
-        left: 10,
-        textStyle: {
-            color: "red",
-            fontSize: 18,
-        },
+            type: 'cross',
+            crossStyle: {
+                color: '#999'
+            }
+        }
     },
     toolbox: {
         feature: {
-            dataZoom: {
-                yAxisIndex: "none",
-            },
-            restore: {},
-            saveAsImage: {},
-        },
+            dataView: {show: true, readOnly: false},
+            magicType: {show: true, type: ['line', 'bar']},
+            restore: {show: true},
+            saveAsImage: {show: true},
+        }
     },
-    axisPointer: {
-        link: { xAxisIndex: "all" },
+    legend: {
+        data: ['Price', 'Bid Count', '平均温度']
     },
-    dataZoom: [
-        {
-            show: true,
-            realtime: true,
-            start: 30,
-            end: 70,
-            xAxisIndex: [0, 1],
-        },
-        {
-            type: "inside",
-            realtime: true,
-            start: 30,
-            end: 70,
-            xAxisIndex: [0, 1],
-        },
-    ],
-    grid: [
-        {
-            left: 50,
-            right: 50,
-            height: "35%",
-        },
-        {
-            left: 50,
-            right: 50,
-            top: "55%",
-            height: "35%",
-        },
-    ],
     xAxis: [
         {
-            type: "category",
-            boundaryGap: false,
-            axisLine: { onZero: false },
-            data: timeData,
-        },
-        {
-            gridIndex: 1,
-            type: "category",
-            boundaryGap: false,
-            axisLine: { onZero: true },
-            data: timeData,
-            position: "top",
-        },
+            type: 'category',
+            data: ['12:00 AM', '1:00 AM', '2:00 AM', '3:00 AM', '4:00 AM', '5:00 AM', '6:00 AM', '7:00 AM', '8:00 AM', '9:00 AM', '10:00 AM', '11:00 AM', '12:00 PM', '1:00 PM', '2:00 PM', '3:00 PM', '4:00 PM', '5:00 PM', '6:00 PM', '7:00 PM', '9:00 PM', '10:00 PM','11:00 PM','12:00 PM'],
+            axisPointer: {
+                type: 'shadow'
+            }
+        }
     ],
-    textStyle: {
-        color: "#FFFF",
-        fontSize: 18,
-    },
     yAxis: [
         {
-            name: "Bids Price",
-            type: "value",
-            max: 1300,
+            type: 'value',
+            name: 'Bid Range',
+            min: 0,
+            max: 100000,
+            interval: 10000,
             splitLine: {
-                show: false,
+                show: false
             },
+            axisLabel: {
+                formatter: '{value}'
+            }
         },
         {
-            gridIndex: 1,
-            name: "Time",
-            type: "value",
-            inverse: true,
+            type: 'value',
+            name: 'Bid Count',
+            min: 0,
+            max: 25,
+            interval: 5,
             splitLine: {
-                show: false,
+                show: false
             },
-        },
+            axisLabel: {
+                formatter: '{value}'
+            }
+        }
     ],
     series: [
         {
-            name: "Bids Price",
-            type: "line",
-            symbolSize: 8,
-            hoverAnimation: false,
-            fill: true,
+            name: 'Price',
+            type: 'line',
+            fill: 'true',
             areaStyle: {
-                color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
-                    {
-                        offset: 1,
-                        color: "#61BCFF",
-                    },
-                    {
-                        offset: 0.5,
-                        color: "#0093FF",
-                    },
-                ]),
+                color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+                    offset: 0,
+                    color: '#2F8FEB'
+                }, {
+                    offset: 1,
+                    color: '#fffF'
+                }])
             },
-            data: [
-                0,
-                100,
-                379,
-                200,
-                100,
-                400,
-                800,
-                100,
-                300,
-                778,
-                290,
-                488,
-                300,
-                0,
-                100,
-                379,
-                778,
-                290,
-                488,
-                300,
-                200,
-                100,
-                400,
-                800,
-                100,
-                300,
-                778,
-                290,
-                488,
-                300,
-            ],
+            data: [10000, 12000,13000, 13050, 13560, 30000, 50000, 50000, 60000, 70000.6, 750000, 750000,80000,60000,70000,50000,40000,37000,70000,78000,78000,69999,40000, 3.3]
         },
         {
-            name: "Bids Count",
-            type: "line",
-            xAxisIndex: 1,
+            name: 'Bid Count',
+            type: 'line',
             yAxisIndex: 1,
-            symbolSize: 8,
-            hoverAnimation: false,
-            textStyle: {
-                color: "red",
-                fontSize: 18,
-            },
-            fill: true,
-            areaStyle: {
-                color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
-                    {
-                        offset: 1,
-                        color: "#F44738",
-                        borderColor: "#F44738",
-                        lineStyle: {
-                            color: "#F44738",
-                        },
-                    },
-                    {
-                        offset: 0.1,
-                        color: "#FF9A89",
-                    },
-                ]),
-            },
-            data: bidsData,
-        },
-    ],
+            data: [2.0, 2.2, 3.3, 4.5, 6.3, 10.2, 20.3, 23.4, 23.0, 16.5, 12.0, 6.2,10,23,21,42,14,56,74,7,40,10,25,]
+        }
+    ]
 };
-
-time = setInterval(function () {}, 60 * 1000);
-
-if (option && typeof option === "object") {
+if (option && typeof option === 'object') {
     myChart.setOption(option);
 }
