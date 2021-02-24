fdc61db358e3f64a01672c78b5328d38f2a5a0cd
Sanjaya Senevirathne <sanjayaharasana@gmail.com>
2021-02-24 17:24:06 +0530

    Project Acition

--- Modules/Projects/Routes/frontend/frontend.php
+++ Modules/Projects/Routes/frontend/frontend.php
@@ -3,3 +3,5 @@
 Route::get('/page/{id}', 'ProjectController@show')->name('project_page');
 Route::get('/explore-project/{category_id}/{search_keyword}/{county}/{bids_range}/{sort_by}', 'ProjectController@index')->name('project_explore');
 Route::get('/apply-project/{project_id}', 'ProjectController@apply_project')->name('apply_project');
+
+Route::get('/auction-project/{project_id}', 'ProjectAuctionController@index')->name('project_auction');
