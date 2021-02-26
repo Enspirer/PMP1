84ffa3a6a914447246b6e2f5777f5b200388e0f0
Sanjaya Senevirathne <sanjayaharasana@gmail.com>
2021-02-26 19:58:38 +0530

    Bids Propsals

---
+++ resources/views/frontend/user/my_projects/tabs_pages/bids_and_proposals.blade.php
@@ -0,0 +1,11 @@
+<div class="container">
+    <div class="row">
+        <h5>Bids and Proposals</h5>
+        <div class="header-search-top ml-auto">
+            <div class="inputWithIcon">
+                <input type="text" placeholder="Search projects" />
+                <i class="fa fa-search" aria-hidden="true"></i>
+            </div>
+        </div>
+    </div>
+</div>
