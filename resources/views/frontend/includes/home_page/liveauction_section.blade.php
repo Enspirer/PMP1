9528e6ecff7d90dc98eb22caae1b147e89c362df
Sanjaya Senevirathne <sanjayaharasana@gmail.com>
2021-03-03 08:50:46 +0530

    Live Auctio Section

--- resources/views/frontend/includes/home_page/liveauction_section.blade.php
+++ resources/views/frontend/includes/home_page/liveauction_section.blade.php
@@ -32,7 +32,7 @@
                     </div>
                     <div class="auction-button" data-animscroll="fade-up">
                         <div class="row" style="margin: 0;display: grid">
-                            <a class="outline-btn-auction" href="#" style="font-size: 16px;background: transparent;color: #0F9D58;text-align: center;font-weight: 500;margin: 0;border: none">PROJECT BRIEF</a>
+                            <a class="outline-btn-auction" type="button"  class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter" style="font-size: 16px;background: transparent;color: #0F9D58;text-align: center;font-weight: 500;margin: 0;border: none">PROJECT BRIEF</a>
                             {{--<a class="outline-btn-auction" href="#" style="margin-top: 5px">VIEW SCOPE</a>--}}
                             <a class="outline-btn-auction" href="" style="margin: 5px 0 0 0;font-size: 16px; background: transparent;color: #0F9D58;text-align: center;font-weight: 500;border: none">VIEW SCOPE</a>
 
@@ -54,3 +54,58 @@
         </div>
     </div>
 </section>
+
+
+
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLongTitle">Project Brief</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div class="card">
+                    <div class="container"><br>
+                        <b>Development of eCommerce platform for a leading Fashion brand</b>
+                        <br><br>
+                        <p>
+                            We are a multinational Currier company who
+                            is planning to get a customized ERP solution
+                            with the following  modules <br>
+                            <br>
+                        </p>
+
+                        <ul>
+                            <li>
+                                Accounting &amp; Financials
+                            </li>
+                            <li>Human Capital Management</li>
+                            <li>Manufacturing &amp; Distribution</li>
+                            <li>Manufacturing &amp; Distribution</li>
+                            <li>Sales &amp; Inventory Management</li>
+                            <li>Supply chain management</li>
+                            <li>Customer relationship management</li>
+                            <li>Support center management</li>
+                            <li>Deployment</li>
+                        </ul>
+                        <br>
+                        <p>We have completed the initial project scope.
+                            The team to undertake the process analysis
+                            and define the sub task management with clear milestones
+                        </p>
+
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+        </div>
+    </div>
+</div>
