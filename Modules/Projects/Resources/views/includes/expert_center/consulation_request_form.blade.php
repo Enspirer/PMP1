5c99667488e65001cccf0651e32f2fd7bfa76e72
Sanjaya Senevirathne <sanjayaharasana@gmail.com>
2021-03-05 12:17:11 +0530

    Consluting Request

--- Modules/Projects/Resources/views/includes/expert_center/consulation_request_form.blade.php
+++ Modules/Projects/Resources/views/includes/expert_center/consulation_request_form.blade.php
@@ -2,18 +2,48 @@
 <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+            <div class="modal-header" style="background-color: purple;">
+                <h5 class="modal-title" id="exampleModalLabel" style="color: white">Request 15min free consultation</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             <div class="modal-body">
-                ...
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save changes</button>
+                <form method="post" action="">
+                    {{csrf_field()}}
+                    <div class="form-group">
+                        <label>Name:</label>
+                        <input type="text" class="form-control" value="">
+                    </div>
+                    <div class="form-group">
+                        <label>Company name:</label>
+                        <input type="text" class="form-control" value="">
+                    </div>
+                    <div class="form-group">
+                        <label>Email:</label>
+                        <input type="text" class="form-control" value="">
+                    </div>
+                    <div class="form-group">
+                        <label>Telephone:</label>
+                        <input type="text" class="form-control" value="">
+                    </div>
+                    <div class="form-group">
+                        <label>Project Brief:</label>
+                        <textarea type="text" class="form-control" rows="5"></textarea>
+                    </div>
+                    <div class="form-group">
+                        <label>Appoint date and time:</label>
+                        <input type="text" class="form-control" value="">
+                    </div>
+                    <div class="form-group">
+                        <label>Additional notes :</label>
+                        <textarea type="text" class="form-control" rows="5"></textarea>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
