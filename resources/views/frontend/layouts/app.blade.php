aac6e7e787a8ada25da858edef3654a6de445646
Sanjaya Senevirathne <sanjayaharasana@gmail.com>
2021-03-06 10:59:26 +0530

    Consulting Controller

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -227,6 +227,20 @@
         <script src="{{url('theme_light/plugin/chart/echarts.min.js')}}"></script>
         <script src="{{url('theme_light/charts/chart_section.js')}}"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/hammer.js/2.0.8/hammer.min.js"></script>
+        
+        <!--Start of Tawk.to Script-->
+        <script type="text/javascript">
+            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+            (function(){
+                var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+                s1.async=true;
+                s1.src='https://embed.tawk.to/6043128b385de407571d2421/1f0300t1e';
+                s1.charset='UTF-8';
+                s1.setAttribute('crossorigin','*');
+                s0.parentNode.insertBefore(s1,s0);
+            })();
+        </script>
+        <!--End of Tawk.to Script-->
 
         <script type="text/javascript">
 
