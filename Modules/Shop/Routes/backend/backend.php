626eabed9de98e3af204613c2bd2023356ced80f
Kavindu Theekshana <kavindutheekshana@gmail.com>
2021-03-22 18:16:53 +0530

    Front End

--- Modules/Shop/Routes/backend/backend.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-Route::get('invoice/show', 'InvoiceController@index')->name('invoice.index');
-
-Route::get('products/index', 'ProductController@index')->name('product.index');
-Route::get('products/show/{id}', 'ProductController@index')->name('product.show');
