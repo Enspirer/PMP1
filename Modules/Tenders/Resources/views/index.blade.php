f092005cb0095f6bceaa33d7ae0fe765dff9292b
Sanjaya Senevirathne <sanjayaharasana@gmail.com>
2021-04-03 00:21:22 +0530

    Tenders Modules Creation

---
+++ Modules/Tenders/Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('tenders::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('tenders.name') !!}
+    </p>
+@endsection
