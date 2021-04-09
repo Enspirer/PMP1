7a74a7f32eadb0a5fe830ca679ab187aa8199419
Sanjaya Senevirathne <sanjayaharasana@gmail.com>
2021-04-09 21:05:56 +0530

    Partner Controller

--- Modules/SupportCenter/Routes/frontend/frontend.php
+++ Modules/SupportCenter/Routes/frontend/frontend.php
@@ -1,4 +1,5 @@
 <?php
 
 Route::get('/contact-us', 'FrontendController@index')->name('contact_us');
+Route::get('/bacome-a-partner', 'PartnerController@index')->name('become_a_partner');
 Route::post('/save-contact-leeds', 'FrontendController@store')->name('contact_us.store');
