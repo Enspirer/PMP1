756ff58f172aaa6c807affea3c97cc46b116fbf0
Kavindu Theekshana <kavindutheekshana@gmail.com>
2021-04-19 13:09:06 +0530

    Project Inner

---
+++ Modules/Projects/Resources/views/mobile/project_page.blade.php
@@ -0,0 +1,225 @@
+<section class="auction-mob-all" style="height: 200px">
+      <div
+        class="dark-layer-mob-shop"
+        style="
+          background: transparent
+            linear-gradient(180deg, #361d44 0%, #4d0088 100%) 0% 0% no-repeat
+            padding-box;
+          opacity: 1;
+        "
+      ></div>
+      <div class="title-group" style="top: 30%">
+        <div class="head-title">
+          <div class="row" data-animscroll="fade-up">
+         
+            <h1>Customized ERP Solution</h1>
+          </div>
+        </div>
+        <h1>Auction ends in : 12Hr 34Min</h1>
+      </div>
+
+      <br /><br />
+    </section>
+
+    <section id="auction-inner">
+
+
+ 
+
+      <br />
+      <h1>Project Brief</h1>
+      <h4>
+        We are a multinational Currier company who is planning to get a customized ERP solution with the following modules
+        <ul>
+            <li><h4> Accounting & Financials</h4></li>
+            <li><h4>Human Capital Management</h4></li>
+            <li><h4>Manufacturing & Distribution</h4></li>
+            <li><h4>Sales & Inventory Management</h4></li>
+            <li><h4>Supply chain management</h4></li>
+            <li><h4> Customer relationship management</h4></li>
+            <li><h4>Support center management Deployment</h4></li>
+        </ul>
+    </h4>
+      <p>We have completed the initial project scope. The team to undertake the process analysis and define the sub task management with clear milestones.</p>
+      <br />
+      <h1>Skill Set</h1>
+
+      <div class="row">
+        <div class="skill2">UI/UX expertiese</div>
+        <div class="skill2">PHP</div>
+        <div class="skill2">HTML/CSS</div>
+        <div class="skill2">Wordpress</div>
+        <div class="skill2">laravel</div>
+      </div>
+      
+
+      <!-- <div class="swiper-container swiper-container-mob7 mt-5">
+        <div class="swiper-wrapper">
+          <div class="swiper-slide">
+            <div class="skill">Wordpress</div>
+          </div>
+          <div class="swiper-slide">
+            <div class="skill">Vector Graphices</div>
+          </div>
+          <div class="swiper-slide">
+            <div class="skill">Figma</div>
+          </div>
+        </div>
+      </div> -->
+
+      <br /><br />
+      <h1>Project Scope & Attachments</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+        eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+        voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+        clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
+        amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+        nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+        sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
+      </p>
+
+      <div class="row row-download">
+        <button>
+          <img src="{{url('theme_light/assets/image/icon/pdf.svg')}}" alt="" />Download project scope
+          part 1
+        </button>
+        <button>
+          <img src="{{url('theme_light/assets/image/icon/pdf.svg')}}" alt="" />Download project scope
+          part 2
+        </button>
+        <button>
+          <img src="{{url('theme_light/assets/image/icon/doc.svg')}}" alt="" />User profiles and
+          journey
+        </button>
+      </div>
+
+      <br />
+      <h1>Client's Preferences</h1>
+      <div class="details-auction-mob-row">
+        <div class="row m-0">
+          <p>Location : &nbsp; <b>Sri Lanka India</b></p>
+        </div>
+        <div class="row m-0">
+          <p>Agency Level : &nbsp; <b>Level 2 or higher</b></p>
+        </div>
+        <div class="row m-0">
+          <p>Job Completion rate : &nbsp; <b>85% or higher</b></p>
+        </div>
+        <div class="row m-0">
+          <p>Language : &nbsp; <b>English Sinhala</b></p>
+        </div>
+        <div class="row m-0">
+          <p>Agency : &nbsp; <b>Startup or Medium company</b></p>
+        </div>
+      </div>
+      <br />
+      <h1>Activity on this Project</h1>
+      <div class="details-auction-mob-row">
+        <div class="row m-0">
+          <p>Bids : &nbsp; <b>10 to 20</b></p>
+        </div>
+        <div class="row m-0">
+          <p>Impressions : &nbsp; <b>120</b></p>
+        </div>
+      </div>
+      <br />
+      <h1>Overview</h1>
+      <div class="details-auction-mob-row">
+        <div class="row m-0">
+          <p>Estimated Budget : &nbsp; <b>Rs. 25,000 - 40,000</b></p>
+        </div>
+        <div class="row m-0">
+          <p>Project Duration :&nbsp; <b>1 - 2 Months</b></p>
+        </div>
+        <div class="row m-0">
+          <p>Project Type :&nbsp; <b>Premium project</b></p>
+        </div>
+        <div class="row m-0">
+          <p>Required Experience level :&nbsp; <b>Intermediate Level</b></p>
+        </div>
+        <div class="row m-0">
+          <p>Payment Category :&nbsp; <b>Fixed Price</b></p>
+        </div>
+      </div>
+      <br />
+      <h1>About Client</h1>
+      <div class="details-auction-mob-row">
+        <div class="row m-0">
+          <i class="fa fa-check-circle" aria-hidden="true"></i>
+          <p>Verified Client</p>
+        </div>
+
+        <div class="row m-0">
+          <i class="fa fa-map-marker" aria-hidden="true"></i>
+          <p>Sri Lanka</p>
+        </div>
+
+        <div class="row m-0">
+          <i class="fa fa-briefcase" aria-hidden="true"></i>
+          <p>8 Projects Posted</p>
+        </div>
+
+        <div class="row m-0">
+          <i class="fa fa-id-card" aria-hidden="true"></i>
+          <p>100% Hire Rate</p>
+        </div>
+
+        <div class="row m-0">
+          <i class="fa fa-usd" aria-hidden="true"></i>
+          <p>Rs. 90k total spend</p>
+        </div>
+
+        <div class="row m-0">
+          <i class="fa fa-star" aria-hidden="true"></i>
+          <p>4.89 from 8 review</p>
+        </div>
+      </div>
+
+      <br><br>
+
+      <a href="#" class="row m-0 green">
+        <i class="fa fa-exclamation-triangle mr-3 green" aria-hidden="true"></i>
+        <p class="green">4.89 from 8 review</p>
+      </a>
+
+      <a href="#" class="row m-0">
+        <i class="fa fa-share-alt mr-3 green" aria-hidden="true"></i>
+        <p class="green">Share this project</p>
+      </a>
+
+      <h1>More Projects</h1>
+      <div class="more">
+          <div class="row">
+              <div class="col-8">
+                  <h1>Update an existing website</h1>
+                  <p>Posted 2h ago</p>
+              </div>
+              <div class="col-4">
+                  <p>USD</p>
+                  <h1>700 - 1000</h1>
+              </div>
+          </div>
+      </div>
+      <br>
+      <h1>Similar Projects from Tallentor</h1>
+      <div class="more">
+        <div class="row">
+            <div class="col-8">
+                <h1>ERP solution for live stock Management</h1>
+                <p>Posted 2h ago</p>
+            </div>
+            <div class="col-4">
+                <p>USD</p>
+                <h1>3000 - 4000</h1>
+            </div>
+        </div>
+    </div>
+    </section>
+
+    <section id="auction-btn-bottum-section-mob">
+      <div class="row">
+        <div class="col-6 pl-3 pr-1"><a class="outline" type="button" href="#">SAVE PROJECT</a></div>
+        <div class="col-6 pl-1 pr-3"><a class="fill" type="button" href="#">APPLY NOW</a></div>
+      </div>
+    </section>
--- Modules/Projects/Resources/views/project_page.blade.php
+++ Modules/Projects/Resources/views/project_page.blade.php
@@ -3,62 +3,11 @@
 @section('title', app_name() . ' | ' . __('labels.frontend.contact.box_title'))
 
 @section('content')
-    @include('projects::includes.project_page.project_header')
-        <div class="body-section-single-project">
-            <div class="container">
-                <div class="row">
-                    <div class="left col-md-8">
-                       @include('projects::includes.project_page.project_description')
-
-                        <h2 class="title-skill">Skill Set</h2>
-                        <div class="row-skill row">
-                            <button>Ui / UX expertise</button>
-                            <button>PHP </button>
-                            <button>HTML / CSS</button>
-                        </div>
-
-                        @auth
-                             @include('projects::includes.project_page.project_scopre')
-                        @else
-
-                        @endauth
-                    </div>
-                    <div class="right col-md-4">
-
-                        @auth
-                             <a href="{{route('frontend.apply_project',1)}}" class="apply-button" style="margin: 10px 0;">APPLY TO THIS PROJECT</a><br><br>
-                            <button class="save-button">
-                                <i class="fa fa-heart" aria-hidden="true"></i> SAVE PROJECT
-                            </button>
-                        @else
-                            <a href="{{route('frontend.auth.login')}}" class="apply-button" style="margin: 10px 0;">APPLY TO THIS PROJECT</a>
-                        @endauth
-
-
-                        <div class="right-card">
-                            @include('projects::includes.project_page.estimete_projects')
-                        </div>
-
-                        @auth
-                            @include('projects::includes.project_page.company_details')
-                        @else
-
-                        @endauth
-
-                        <div class="bottum-buttons">
-                            <button class="button">
-                                <i class="fa fa-exclamation-triangle" aria-hidden="true"></i> Report this project
-                            </button>
-                            <button class="button">
-                                <i class="fa fa-share-alt" aria-hidden="true"></i> Share this project
-                            </button>
-                        </div>
-                    </div>
-                </div>
-                @include('projects::includes.project_page.suggest_more_project')
-            </div>
-        </div>
-    </div>
+@if(is_mobile(request()->header('user-agent')) != true)
+        @include('projects::desktop.project_page')
+    @else
+    @include('projects::mobile.project_page')
+    @endif
 @endsection
 
 @push('after-scripts')
