04409075d80df9e876aa8ba4846616c2bcdb2303
imzajjithvedha <zajjith@gmail.com>
2021-08-27 14:12:36 +0530

    icon pack

--- resources/views/frontend/includes/home_page/tallentor_mall.blade.php
+++ resources/views/frontend/includes/home_page/tallentor_mall.blade.php
@@ -8,14 +8,48 @@
 
 <div class="container text-center" style="margin-bottom: 7rem">
 
-    <div class="heading-text">
-        <h1 style="font-size: 42px; color: #361d41; margin: auto; text-align: center; font: normal normal bold 48px/70px Poppins;">Tallentor Mall</h1>
+    <div class="row mb-5 justify-content-center">
+        <div class="col-6">
+            <div class="row align-items-center">
+                <div class="col-3">
+                    <img src="{{ url('theme_light/assets/tallentor_mall/tallentor_mall_logo.jpg') }}" alt="" class="img-fluid">
+                </div>
+                <div class="col-9">
+                    <h1 style="font-size: 42px; color: #361d41; margin: auto; text-align: center; font: normal normal bold 48px/70px Poppins;">Tallentor Mall</h1>
+                </div>
+            </div>
+        </div>
+        
     </div>
     
 
     <div class="row">
         <div class="col-4">
-            <img src="" alt="">
+            <img src="{{ url('theme_light/assets/tallentor_mall/traditional_modern_musk.jpg') }}" alt="" class="img-fluid">
         </div>
+
+        <div class="col-4">
+            <img src="{{ url('theme_light/assets/tallentor_mall/traditional_modern_musk.jpg') }}" alt="" class="img-fluid">
+        </div>
+
+        <div class="col-4">
+            <img src="{{ url('theme_light/assets/tallentor_mall/traditional_modern_musk.jpg') }}" alt="" class="img-fluid">
+        </div>
+
+    </div>
+
+    <div class="row">
+        <div class="col-4">
+            <img src="{{ url('theme_light/assets/tallentor_mall/traditional_modern_musk.jpg') }}" alt="" class="img-fluid">
+        </div>
+
+        <div class="col-4">
+            <img src="{{ url('theme_light/assets/tallentor_mall/traditional_modern_musk.jpg') }}" alt="" class="img-fluid">
+        </div>
+
+        <div class="col-4">
+            <img src="{{ url('theme_light/assets/tallentor_mall/traditional_modern_musk.jpg') }}" alt="" class="img-fluid">
+        </div>
+        
     </div>
 </div>
