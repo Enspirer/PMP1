f2004f137be32b89d154cfe19e8d7e92683d48da
sanjayaharshana <sanjayaharasana@gmail.com>
2021-09-02 08:37:50 +0530

    Category Function

--- resources/views/frontend/includes/home_page/categories.blade.php
+++ resources/views/frontend/includes/home_page/categories.blade.php
@@ -29,7 +29,6 @@ button:focus {
 }
 </style>
 
-<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 @endpush
 
 <div class="container text-center" style="margin-top: 7rem; margin-bottom: 10rem">
