c0cab72c174583721070f2223d3a36827fd570b4
sanjayaharshana <sanjayaharasana@gmail.com>
2021-11-13 12:56:36 +0530

    Blog Post Blades

--- Modules/Blog/Resources/views/frontend/desktop/blog_post.blade.php
+++ Modules/Blog/Resources/views/frontend/desktop/blog_post.blade.php
@@ -1,6 +1,23 @@
+<style>
+    #news-section .top-part-news .container .news-body p{
+        line-height:inherit;
+    }
+</style>
+
+
+
 <section class="expert-section" style="padding: 90px;">
+    <div class="container">
+        <nav aria-label="breadcrumb" style="margin-top: 50px;background-color: white !important;">
+            <ol class="breadcrumb" style="background-color: white !important;">
+                <li class="breadcrumb-item"><a href="#">Blog</a></li>
+                <li class="breadcrumb-item"><a href="#">{{Modules\Blog\Entities\BlogCategory::where('id',$get_post->category_id)->first()->name}}</a></li>
+                <li class="breadcrumb-item active" aria-current="page">{{$get_post->title}}</li>
+            </ol>
+        </nav>
+    </div>
         <div class="expert-image" style="">
-            <img src="{{url($get_post->feature_image)}}" style="margin-top: 100px;object-position: top;object-fit: contain;" alt="" />
+            <img src="{{url($get_post->feature_image)}}" style="object-position: top;object-fit: contain;" alt="" />
             <div class="expert-text-head">
                 <div class="container"></div>
             </div>
@@ -10,10 +27,13 @@
     <section id="news-section">
         <div class="top-part-news">
             <div class="container">
+
                 <div class="title-part">
                     <div class="row m-0">
                         <p>Post {{$get_post->created_at}} </p>
                         <p>By {{$author_details->first_name}} {{$author_details->last_name}}</p>
+
+
                     </div>
                     <h1>{{$get_post->title}}</h1>
                 </div>
